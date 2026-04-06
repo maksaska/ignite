7df5df1513134b79cc3ae4714fa185ddcfd58dd5
Maksim Davydov <davydov.m.m@sbertech.ru>
2026-04-06 09:55:44 +0300

    IGNITE-28362 Fix unmodifiableList

--- modules/core/src/main/java/org/apache/ignite/internal/processors/platform/client/cache/ImmutableArrayMap.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/platform/client/cache/ImmutableArrayMap.java
@@ -20,6 +20,7 @@ package org.apache.ignite.internal.processors.platform.client.cache;
 import java.io.Serializable;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -61,7 +62,7 @@ public class ImmutableArrayMap<K, V> implements Map<K, V>, Serializable {
         assert keysArr.length == valuesArr.length : "Arrays should be equal in size!";
 
         this.keys = new ImmutableArraySet<>(keysArr);
-        this.values = Arrays.asList(valuesArr);
+        this.values = Collections.unmodifiableList(Arrays.asList(valuesArr));
     }
 
     /** {@inheritDoc} */
